c28bc5296716b0b169d5bdeb7d57558e73bc664a
Theo Dao <trungdao912@gmail.com>
2020-07-28 14:10:56 +0700

    adding custom error HOC

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import withErrorHandler from './hoc/withErrorHandler';
 import Login from './routes/Login';
 import Signup from './routes/Signup';
 import Dashboard from './routes/Dashboard';
@@ -11,7 +12,7 @@ function App() {
         <Switch>
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/dashboard" component={withErrorHandler(Dashboard)} />
           <Route path="/" component={() => <Redirect to="/dashboard" />} />
           <Route />
         </Switch>
---
+++ src/hoc/withErrorHandler/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Http from '../../utils/HttpUtils';
+
+const withErrorHandler = (WrappedComponent) => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+      };
+    }
+
+    componentDidMount() {
+      this.reqInterceptor = Http.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      this.resInterceptor = Http.interceptors.response.use(
+        (res) => {
+          if (typeof res.data !== 'object') {
+            Promise.reject(res);
+          }
+
+          return res;
+        },
+        (err) => {
+          /** Custom behavior of error status code here  */
+          return Promise.reject(err.response);
+        },
+      );
+    }
+
+    componentDidCatch(error) {
+      this.setState({ error });
+    }
+
+    componentWillUnmount() {
+      Http.interceptors.request.eject(this.reqInterceptor);
+      Http.interceptors.response.eject(this.resInterceptor);
+    }
+
+    render() {
+      return (
+        <>
+          <WrappedComponent />
+        </>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
