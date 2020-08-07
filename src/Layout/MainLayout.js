9a7bbd590dbbc3fef86771c65f62fee0bee39012
Theo Dao <trungdao912@gmail.com>
2020-08-08 01:39:39 +0700

    remove unused variable

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Loader, TopMenu } from '@gotitinc/design-system';
 import { connect } from 'react-redux';
